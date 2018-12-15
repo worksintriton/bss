04c6d354bf6046d0eb3fa02fb2d1acae0dde5cf0
Hari <worksintriton@gmail.com>
2018-12-15 12:58:57 +0530

    asdf

--- services/point_tracking.js
+++ services/point_tracking.js
@@ -491,7 +491,7 @@ point_tracking.FetchMapSpotrecordmobile = function (userInput, resultCallback) {
 
 point_tracking.addmapuseweb = function (userInput, resultCallback) {
   var executor = db.getdaata.getdb();
-  executor.one('INSERT INTO public."Mapusers"("Emp_id","Employee_name","Map_id","gender","contact_no","Email_id","Client_place","Address")VALUES($1,$2,$3,$4,$5,$6,$7,$8)RETURNING *',
+  executor.one('INSERT INTO public."Mapusers"("Emp_id","Employee_Name","Map_id","gender","contact_no","Email_id","Address","title")VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9)RETURNING *',
                  [ 
                  userInput.Emp_id,
                  userInput.Employee_name,
@@ -500,7 +500,8 @@ point_tracking.addmapuseweb = function (userInput, resultCallback) {
                  userInput.contact_no,
                  userInput.Email_id,
                  userInput.Client_place,
-                 userInput.Address
+                 userInput.Address,
+                 userInput.Client_place
                  ])
                  .then(data => {
                     resultCallback(null,data);
@@ -554,23 +555,23 @@ point_tracking.fetchmapuserpointsweb1 = function (userInput, resultCallback) {
 
 
 point_tracking.fetchmapuserpointsweb2 = function (userInput, resultCallback) {
-  // var executor = db.getdaata.getdb();
-  // executor.any('Select * from public."Mapusers" WHERE "Emp_id"=($1) ',[userInput.Emp_id])
-  //                .then(data => {
-  //                   resultCallback(null,data);
-  //                })
-  //                .catch(error => {
-  //                   resultCallback(null,error );
-  //               })
-
-    var executor = db.getdaata.getdb();
-  executor.any('Select * from public."PointTrackMap" WHERE ukey in (Select CAST ("Map_id"  AS INTEGER) from public."Mapusers" WHERE "Emp_id"=($1)) ',[userInput.Emp_id])
+  var executor = db.getdaata.getdb();
+  executor.any('Select * from public."Mapusers" WHERE "Emp_id"=($1) ',[userInput.Emp_id])
                  .then(data => {
                     resultCallback(null,data);
                  })
                  .catch(error => {
                     resultCallback(null,error );
                 })
+
+    // var executor = db.getdaata.getdb();
+    // executor.any('Select * from public."PointTrackMap" WHERE ukey in (Select CAST ("Map_id"  AS INTEGER) from public."Mapusers" WHERE "Emp_id"=($1)) ',[userInput.Emp_id])
+    //              .then(data => {
+    //                 resultCallback(null,data);
+    //              })
+    //              .catch(error => {
+    //                 resultCallback(null,error );
+    //             })
 };
 
 
