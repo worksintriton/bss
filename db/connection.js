0ef217b028fd54cda3908835e14ba547076654f8
Hari <worksintriton@gmail.com>
2019-01-03 15:39:04 +0530

    server migiration

--- db/connection.js
+++ db/connection.js
@@ -18,12 +18,12 @@ monitor.setLog((msg, info) => {
 var db;
 function getConnection() {
     const cn = {
-	  user: "rgrsempisxrbqu",
-	  password: "c536c891f3beac96c241c0646919a0d4284ec99633f460e104a5b90d043a4bd4",
-	  database: "dcas8tnojgluv5",
+	  user: "bssuser",
+	  password: "bssuser@2019",
+	  database: "BSS",
 	  port: 5432,
-	  host: "ec2-107-21-233-72.compute-1.amazonaws.com",
-	  ssl: true
+	  host: "132.148.140.42",
+	  ssl: false
 	};
 	db = pgp(cn); 
     return db;
