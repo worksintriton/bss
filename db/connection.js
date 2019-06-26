4517b35f5079a7250cca97f5d7e1fd5d38ce09fd
KartheKeyan D <tdkarthekeyan@gmail.com>
2019-06-26 17:21:50 +0530

    commit by karthi

--- db/connection.js
+++ db/connection.js
@@ -1,36 +1,35 @@
 "use strict";
 
-const promise = require('bluebird'); // or any other Promise/A+ compatible library;
+const promise = require("bluebird"); // or any other Promise/A+ compatible library;
 
 const initOptions = {
-    promiseLib: promise // overriding the default (ES6 Promise);
+  promiseLib: promise // overriding the default (ES6 Promise);
 };
 
-const pgp = require('pg-promise')(initOptions);
-const monitor = require('pg-monitor');
+const pgp = require("pg-promise")(initOptions); // connect db
+const monitor = require("pg-monitor");
 
 monitor.attach(initOptions); // attach to all query events;
 
-monitor.setTheme('matrix'); // change the default theme;
+monitor.setTheme("matrix"); // change the default theme;
 
-monitor.setLog((msg, info) => {
-});
+monitor.setLog((msg, info) => {});
 var db;
 function getConnection() {
-    const cn = {
-	  user: "bssuser",
-	  password: "bssuser@2019",
-	  database: "BSS",
-	  port: 5432,
-	  host: "132.148.140.42",
-	  ssl: false
-	};
-	db = pgp(cn); 
-    return db;
+  const cn = {
+    user: "bssuser",
+    password: "bssuser@2019",
+    database: "BSS",
+    port: 5432,
+    host: "132.148.140.42",
+    ssl: false
+  };
+  db = pgp(cn);
+  return db;
 }
 
 function getdb() {
-    return db;
+  return db;
 }
 
 exports.getdb = getdb;
