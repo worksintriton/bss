67061554b0ba8eeb35dc0101c42772362a13f3a1
pradeep-sigamani <pradeep@tritonadc.in>
2024-01-24 11:52:52 +0530

    updated model and node modules

--- index.js
+++ index.js
@@ -1,41 +1,57 @@
 "use strict";
 
-var db = require("./db"),
-    path = require("path"),
-    api = require("./api"),
-    config = require("config"),
-    utils = require("./utils"),
-    express = require("express"),
-    bodyParser = require("body-parser"),
-    cookieParser = require("cookie-parser"),
-    expressSession = require("express-session"),
-    fs = require('fs'),
-    stripWhitespace = require('strip-whitespace'),
-    https = require('https'),
-    middleware = require("./api/middleware"),
-    cors = require("cors");
+// var db = require("./db"),
+(path = require("path")),
+  (api = require("./api")),
+  (config = require("config")),
+  (utils = require("./utils")),
+  (express = require("express")),
+  (bodyParser = require("body-parser")),
+  (cookieParser = require("cookie-parser")),
+  (expressSession = require("express-session")),
+  (fs = require("fs")),
+  (stripWhitespace = require("strip-whitespace")),
+  (https = require("https")),
+  (middleware = require("./api/middleware")),
+  (cors = require("cors"));
+
+const db = require("./db/database");
 
 var app = express();
 
-
-var jsonParser = bodyParser.json({limit: 1024 * 1024 * 20, type: 'application/json'});
-var urlencodedParser = bodyParser.urlencoded({extended: true, limit: 1024 * 1024 * 20, type: 'application/x-www-form-urlencoding'})
+var jsonParser = bodyParser.json({
+  limit: 1024 * 1024 * 20,
+  type: "application/json",
+});
+var urlencodedParser = bodyParser.urlencoded({
+  extended: true,
+  limit: 1024 * 1024 * 20,
+  type: "application/x-www-form-urlencoding",
+});
 
 app.use(jsonParser);
 app.use(urlencodedParser);
 
-app.use("*", [cors(),middleware.passport.initialize(), middleware.passport.session()]);
-app.options('*', cors());
+app.use("*", [
+  cors(),
+  middleware.passport.initialize(),
+  middleware.passport.session(),
+]);
+app.options("*", cors());
 
 app.use(cookieParser());
-app.use(expressSession({
+app.use(
+  expressSession({
     secret: "Flatmanagement",
     resave: true,
-    saveUninitialized: true
-}));
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+    saveUninitialized: true,
+  })
+);
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 app.use(bodyParser.json());
 
 app.use("/", express.static(path.join(__dirname, "www")));
@@ -47,14 +63,13 @@ app.use(api.router);
     res.json(utils.errors["500"]);
 });*/
 
-
 function runServer() {
-    db.init();
-    api.init();
-    var port = process.env.PORT || 84;
-    app.listen(port, function () {
-        console.info("Application listening on port " + port);
-    });
+  db();
+  api.init();
+  var port = process.env.PORT || 84;
+  app.listen(port, function () {
+    console.info("Application listening on port " + port);
+  });
 }
 
 runServer();
